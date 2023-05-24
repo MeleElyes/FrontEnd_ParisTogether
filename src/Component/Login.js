60c3876b42b3add5799369a91e9d5028848e36bf
MeleElyes <118779573+MeleElyes@users.noreply.github.com>
2023-05-24 17:43:35 +0200

    Update Login.js

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -28,7 +28,7 @@ const Login = () => {
     setEmailExist(false);
     setDifferentPassword(false);
     await axios
-      .post("http://localhost:8000/request/login", login)
+      .post("back-end-paris-together-mph8p9fgf-meleelyes.vercel.app/request/login", login)
       .then((res) => {
         localStorage.setItem("auth-token", res.data);
         navigate("/postlogin");
